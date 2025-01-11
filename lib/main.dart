fee9b0da98314899bf17b6f96ac13004093fed22
ForcesR2N <muhammadzuhrizal07@gmail.com>
2025-01-11 08:05:53 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-
   runApp(const MainApp());
 }
 
